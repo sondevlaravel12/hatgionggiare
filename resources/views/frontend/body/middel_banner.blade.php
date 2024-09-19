5ec22397d2e7142719f6e8916e456dd6d3ba88a4
sondc <nguyenminhlamson@gmail.com>
2024-09-19 14:38:06 +0700

    fix LCP with lazy load img

--- resources/views/frontend/body/middel_banner.blade.php
+++ resources/views/frontend/body/middel_banner.blade.php
@@ -3,7 +3,7 @@
 
     <div class="col-sm-6">
       <div class="wide-banner cnt-strip">
-        <div class="image"> <img class="img-responsive" src="{{$banner->getFirstImageUrl()}}" alt="">
+        <div class="image"> <img class="img-responsive" src="{{$banner->getFirstImageUrl()}}" alt="" loading="lazy">
         </div>
       </div>
       <!-- /.wide-banner -->
